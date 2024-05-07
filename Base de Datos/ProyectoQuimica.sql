35b2f04ec264507f805fcf165d877f869a7ccc8f
TomasRM <trobmor655@ieszaidinvergeles.org>
2024-05-07 13:58:30 +0200

    Edit 01 DataBase

--- Base de Datos/ProyectoQuimica.sql
+++ Base de Datos/ProyectoQuimica.sql
@@ -2,23 +2,27 @@
 CREATE DATABASE IF NOT EXISTS QUIMICOS;
 USE QUIMICOS;
 
--- Estructura de tabla para la tabla Usuario
+---- Estructura de tabla para la tabla Usuario
 
+--DROP TABLE IF EXISTS Usuario;
+--CREATE TABLE Usuario (
+--	idUsuario INT PRIMARY KEY IDENTITY, -- El IDENTITY es igual al AUTO_INCREMENT de MySQL
+--	nombreUsuario VARCHAR(255) UNIQUE NOT NULL,
+--	contraseña VARCHAR(255) NOT NULL,
+--	nombre VARCHAR(255) NOT NULL,
+--	apellidos VARCHAR(255) NOT NULL,
+--	esProfesor BIT NOT NULL -- BIT == BOOLEAN en MySQL
+--); 
 
---La tabla usuario puede que no nos haga falta porque ya
--- el propio mysql te trae un sistema de gestión de usuarios que ya he programao en java
-DROP TABLE IF EXISTS Usuario;
-CREATE TABLE Usuario (
-	idUsuario INT PRIMARY KEY IDENTITY,
-	nombreUsuario VARCHAR(255) UNIQUE NOT NULL,
-	contraseña VARCHAR(255) NOT NULL,
-	nombre VARCHAR(255) NOT NULL,
-	apellidos VARCHAR(255) NOT NULL,
-	esProfesor BIT NOT NULL -- BIT == BOOLEAN
-); 
+--Creación de la tabla de Localizacion
 
---Nos falta una tabla de localización que hace referencia a los distintos almacenes
-
+DROP TABLE IF EXISTS Localizacion;
+CREATE TABLE Localizacion (
+	idLocalizacion INT PRIMARY KEY IDENTITY,
+	Almacen VARCHAR(255) NOT NULL,
+	CONSTRAINT CK_Almacen
+        CHECK (Almacen IN ('Laboratorio001', 'Laboratorio002', 'Laboratorio003' 'Laboratorio004', 'Almacen General')),
+);
 
 -- Creación de la tabla de Ubicacion
 
@@ -35,15 +39,14 @@ DROP TABLE IF EXISTS ProductoQuimico;
 CREATE TABLE ProductoQuimico (
 	idProducto INT PRIMARY KEY IDENTITY,
 	nombre VARCHAR(255) NOT NULL,
-	localizacion VARCHAR(50) NOT NULL,
+	idLocalizacion INT NOT NULL,
 	ubicacionCodigo VARCHAR(10),
 	idUbicacion INT,
 	cantidad INT NOT NULL,
 	stockMinimo INT,
-	FOREIGN KEY (idUbicacion) REFERENCES Ubicacion(idUbicacion)
+	FOREIGN KEY (idUbicacion) REFERENCES Ubicacion(idUbicacion),
+	FOREIGN KEY (idLocalizacion) REFERENCES Localizacion(idLocalizacion)
 );
-
--- Nos falta la table de productos auxiliares, que es la tercera especialización de productoQuimico
 
 --Creación de la tabla Reactivo
 
@@ -52,7 +55,6 @@ CREATE TABLE Reactivo (
 	idReactivo INT PRIMARY KEY IDENTITY,
 	idProducto INT NOT NULL,
 	descripcion VARCHAR(255),
-	riesgo VARCHAR(255),
 	gradoPureza VARCHAR(50),
 	fechaCaducidad DATE,
 	FOREIGN KEY (idProducto) REFERENCES ProductoQuimico(idProducto)
@@ -67,9 +69,20 @@ CREATE TABLE Material (
 	Subcategoria VARCHAR(50) NOT NULL,
     CONSTRAINT CK_Subcategoria 
         CHECK (Subcategoria IN ('Plástico', 'Cristal', 'Instrumental Electrónico')),
+	--El ENUM en MySQL sería: nombre_enum ENUM('Plástico', 'Cristal', 'Instrumental Electrónico'),
 	descripcion VARCHAR(255),
 	numeroSerie VARCHAR(100),
 	fechaCompra DATE,
+	FOREIGN KEY (idProducto) REFERENCES ProductoQuimico(idProducto)
+);
+
+--Estructura de la tabla para la tabla Productos Auxiliares
+
+DROP TABLE IF EXISTS ProductoAuxiliar;
+CREATE TABLE ProductoAuxiliar (
+	idProductoAuxiliar INT PRIMARY KEY IDENTITY,
+	idProducto INT NOT NULL,
+	formato VARCHAR(255) NOT NULL,
 	FOREIGN KEY (idProducto) REFERENCES ProductoQuimico(idProducto)
 );
 
@@ -78,7 +91,7 @@ DROP TABLE IF EXISTS Riesgo;
 CREATE TABLE Riesgo (
 	idRiesgo INT PRIMARY KEY IDENTITY,
 	nombre VARCHAR(255) NOT NULL,
-	pictograma VARBINARY(MAX) --Campo para almacenar imágenes
+	pictograma VARBINARY(MAX) --Campo para almacenar imágenes en MySQL es un BLOB en vez de VARBINARY
 );
 
 --Tabla de ReactivoRiesgo (Tabla de unión)
