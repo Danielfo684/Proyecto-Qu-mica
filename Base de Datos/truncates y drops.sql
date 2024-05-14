0a615fc4ca1d18e52f6fae6bf08e05f6042e48e8
Daniel Fontalva Ostio <dfonost684@ieszaidinvergeles.org>
2024-05-14 17:58:02 +0200

    codigo sql extra

---
+++ Base de Datos/truncates y drops.sql
@@ -0,0 +1,24 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+
+TRUNCATE TABLE formato;
+TRUNCATE TABLE localizacion;
+TRUNCATE TABLE material;
+TRUNCATE TABLE productoauxiliar;
+TRUNCATE TABLE productos;
+TRUNCATE TABLE reactivo;
+TRUNCATE TABLE reactivoriesgo;
+TRUNCATE TABLE riesgo;
+TRUNCATE TABLE ubicacion;
+
+
+DROP TABLE formato;
+DROP TABLE localizacion;
+DROP TABLE material;
+DROP TABLE productoauxiliar;
+DROP TABLE productos;
+DROP TABLE reactivo;
+DROP TABLE reactivoriesgo;
+DROP TABLE riesgo;
+DROP TABLE ubicacion;
+
